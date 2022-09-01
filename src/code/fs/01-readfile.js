27c9f6b42c7e6effed22fc91b0c4a7fd55bc8d7e
HexOr <676658978@qq.com>
2022-09-02 00:06:47 +0800

    feat: 同步读取文件

--- src/code/fs/01-readfile.js
+++ src/code/fs/01-readfile.js
@@ -20,3 +20,17 @@ fs.readFile(path.join(__dirname, '../../files/input.txt'), 'utf8', (err, dataStr
   }
   console.log('读取文件成功！' + dataStr);
 });
+
+/**
+ * fs.readFileSync() 同步读取文件
+ * param1: 要读取文件的存放路径
+ * param2: 读取文件时候采用的编码格式（可选，默认二进制）
+ *  如果文件读取成功 返回文件内容
+ *  如果文件读取失败 抛出异常
+ */
+try {
+  const result = fs.readFileSync(path.join(__dirname, '../files/input.txt'), 'utf8');
+  console.log('result: ', result);
+} catch (error) {
+  console.log('读取文件失败！' + error.message);
+}
