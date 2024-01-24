4ac8c18218d4ff89cba31f28b0c6049d208006ff
HexOr <676658978@qq.com>
2024-01-24 08:24:36 +0000

    feat: 增加path的方法

--- src/code/path/index.js
+++ src/code/path/index.js
@@ -14,10 +14,10 @@ console.log('pathB: ', pathB);
 
 /**
  * path.basename(path, extension) 获取文件路径的文件名部分
- *  path:这是用于提取文件名的文件路径。
+*   path:这是用于提取文件名的文件路径。
  *  extension:它是一个可选的文件扩展名，将从返回的字符串中删除扩展名
  */
-const filename = path.basename('./index.js', '.js');
+const filename = path.basename('/abc/index.js', '.js');
 console.log('filename: ', filename);
 
 /**
@@ -26,3 +26,16 @@ console.log('filename: ', filename);
  */
 const extname = path.extname('./index.js');
 console.log('extname: ', extname);
+
+/**
+ * path.dirname(path, extension) 获取文件路径的目录名称
+ */
+const dirname = path.dirname('/abc/efg/index.js');
+console.log('dirname: ', dirname);
+
+/**
+ * path.resolve([...paths]) 将一系列路径或路径段解析为绝对路径
+ * 如果没有path传递任何段，path.resolve()将返回当前工作目录的绝对路径。
+ */
+const resolve = path.resolve('wwwroot', 'static_files/png/', '../gif/image.gif');
+console.log('resolve: ', resolve);
