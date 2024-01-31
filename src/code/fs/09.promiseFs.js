65851c6cde588140ebb899cd9ab2d899814e0022
HexOr <676658978@qq.com>
2024-01-31 06:12:46 +0000

    feat: fsPromises

---
+++ src/code/fs/09.promiseFs.js
@@ -0,0 +1,23 @@
+const fsPromises = require('fs/promises');
+const path = require('path');
+
+const init = async () => {
+  try {
+    // 创建一个控制器对象,通过使用AbortSignal对象完成与异步操作的通信
+    const controller = new AbortController();
+    // 返回一个AbortSignal对象实例，可用于与异步操作通信或中止异步操作（只读）
+    const { signal } = controller;
+    const promise = fsPromises.readFile(path.resolve(__dirname, '../../files/input.txt'), { signal });
+
+    // Abort the request before the promise settles.
+    // controller.abort();
+
+    const data = await promise;
+    console.log(data.toString());
+  } catch (err) {
+    // When a request is aborted - err is an AbortError
+    console.error(err);
+  }
+}
+
+init();
